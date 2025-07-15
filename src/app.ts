2a20280b7aa5b4153483460e09d9f370ac2fd6bd
Ubaidullah-Hasan <hm2964133@gmail.com>
2025-07-15 07:37:48 +0600

    design complete

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,18 @@ import path from "path";
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://example.com',
-  optionsSuccessStatus: 200 
-}))
+app.use(
+  cors({
+    origin: "http://example.com",
+    optionsSuccessStatus: 200,
+  }),
+);
 
-app.use("/",express.static(path.join(process.cwd(), "public"))); // name must index.html
-
+// Static files middleware (public folder)
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -3,34 +3,32 @@ import app from "./app";
 import config from "./app/config/config";
 import { Server } from "http";
 
-
-
 let server: Server;
 
 async function main() {
-    server = app.listen(config.port, () => {
-        console.log(`Server running on 🔗🔗http://localhost:${config.port}`);
-    });
-    await mongoose.connect(config.DATABASEURL);  
-    console.log("Mongodb connected");
+  server = app.listen(config.port, () => {
+    console.log(`Server running on 🔗🔗http://localhost:${config.port}`);
+  });
+  await mongoose.connect(config.DATABASEURL);
+  console.log("Mongodb connected");
 }
- 
+
 main();
 
 process.on("unhandledRejection", () => {
-    console.log("Unhandled rejection is detected, Sutting down...");
-    if (server) {
-        server.close(() => {
-            process.exit(1);
-        })
-    }
-    process.exit(1);
+  console.log("Unhandled rejection is detected, Sutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
 });
 
 process.on("uncaughtException", () => {
-    console.log("UncaughtException rejection is detected, Sutting down...");
-    process.exit(1);
-})
+  console.log("UncaughtException rejection is detected, Sutting down...");
+  process.exit(1);
+});
 
 // process.on('SIGTERM', () => {
 //     console.log('SIGTERM IS RECEIVE');
